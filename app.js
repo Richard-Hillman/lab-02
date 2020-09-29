d21a9da1c28cc6b8ff747b86b149a618bbcbb0e2
Richard Hillman <Richard.CC.Hillman@gmail.com>
2020-09-29 14:24:53 -0700

    working element grabs

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+const userGuess = document.getElementById('user-guess');
+
+const guessButton = document.getElementById('guess-button');
+
+const remGuesses = document.getElementById('rem-guesses');
+
+const higherLower = document.getElementById('higher-lower');
+
+const mysteryNumber = document.getElementById('mystery-number');
+
+console.log(userGuess, guessButton, remGuesses, higherLower, mysteryNumber);
 // import functions and grab DOM elements
 
 // initialize state
