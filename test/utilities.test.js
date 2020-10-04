1a37060a85326bc89f27a6b9d15afd050245dd61
Richard Hillman <Richard.CC.Hillman@gmail.com>
2020-10-04 15:03:53 -0700

    background image added, main section added border

--- test/utilities.test.js
+++ test/utilities.test.js
@@ -65,3 +65,25 @@ test('I want to compare random number with user, and if random number greater th
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
+
+
+
+// TEST RANDOMNUMBER ----------------------------------------------------------------
+
+// IMPORT MODULES under test here:
+// import { example } from '../example.js';
+
+
+// test('time to test a function', (expect) => {
+//     //Arrange
+//     // Set up your arguments and expectations
+//     const expected = true;
+    
+//     //Act 
+//     // Call the function you're testing and set the result to a const
+//     const actual = false;
+
+//     //Expect
+//     // Make assertions about what is expected versus the actual result
+//     expect.equal(actual, expected);
+// });
